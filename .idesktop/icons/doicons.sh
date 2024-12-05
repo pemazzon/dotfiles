052a8f838d45063ea8d087acc93d8f472426be30
Paolo E. Mazzon <paoloemilio.mazzon@gmail.com>
2024-12-05 10:49:27 +0100

    better script

--- .idesktop/icons/doicons.sh
+++ .idesktop/icons/doicons.sh
@@ -1,8 +1,35 @@
 #!/bin/bash
+#
+# version 1.0
+#
+# added the 'Caption:' management
+# you can choose between the simpler form
+# 
+# icon.png
+# Command[0]: execute something
+#
+# or the form with a caption added
+#
+# icon.png
+# Caption: this runs a nice program
+# Command[0]: execute something
+#
+# MANDATORY: put the Caption between
+# the icon specification and the Command one
+#
+# Note the nice trick for the 'Command' directive:
+# by using read / echo with "-e" you can define
+# multiple Commands in one line by using something like
+#
+# Command[0]: startvm BlissOS;\\n    Command[1]: stopvm BlissOS;
+#
+# please preserve the double backslashed \\n 
+# and the four spaces before Command[1]
+
 basediricons=/locale/paolo/.idesktop/icons
 \rm -f $basediricons/../*.lnk
 num=1
-x=3814
+x=1894
 y=36
 iconsize=24
 
@@ -19,8 +46,17 @@ then
    fi
    echo "table Icon" >../${link}
    echo "    Icon: ${basediricons}/$icon" >>../${link}
-   read command
-   echo "    $command" >>../${link}
+
+
+   read -e command
+   echo -e "    $command" >>../${link}
+   if [ $(expr match "$command" '^Caption:') -eq 8 ];
+   then
+      # That was actually a caption...
+      # reading command now
+      read -e command
+      echo -e "    $command" >>../${link}
+   fi
    echo "    Width:  $iconsize" >>../${link}
    echo "    Height: $iconsize" >>../${link}
    echo "    X: $x" >>../${link}
