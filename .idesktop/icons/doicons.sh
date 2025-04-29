26fdb6bf675833f2ce050702ff8c0dd09ec55edd
Paolo E. Mazzon <paoloemilio.mazzon@gmail.com>
2025-04-29 09:07:10 +0200

    better script to generate lnk files

--- .idesktop/icons/doicons.sh
+++ .idesktop/icons/doicons.sh
@@ -1,4 +1,31 @@
 #!/bin/bash
+#
+# version 1.0
+#
+# added the 'Caption:' management
+# you can choose between the simpler form
+# 
+# icon.png
+# Command[0]: execute something
+#
+# or the form with a caption added
+#
+# icon.png
+# Caption: this runs a nice program
+# Command[0]: execute something
+#
+# MANDATORY: put the Caption between
+# the icon specification and the Command one
+#
+# Note the nice trick for the 'Command' directive:
+# by using read / echo with "-e" you can define
+# multiple Commands in one line by using something like
+#
+# Command[0]: startvm BlissOS;\\n    Command[1]: stopvm BlissOS;
+#
+# please preserve the double backslashed \\n 
+# and the four spaces before Command[1]
+
 basediricons=/locale/paolo/.idesktop/icons
 \rm -f $basediricons/../*.lnk
 num=1
@@ -31,6 +58,13 @@ then
 
    read -e command
    echo -e "    $command" >>../${link}
+   if [ $(expr match "$command" '^Caption:') -eq 8 ];
+   then
+      # That was actually a caption...
+      # reading command now
+      read -e command
+      echo -e "    $command" >>../${link}
+   fi
    echo "    Width:  $iconsize" >>../${link}
    echo "    Height: $iconsize" >>../${link}
    echo "    X: $x" >>../${link}
